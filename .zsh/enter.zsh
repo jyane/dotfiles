8610f8c616f05517b88c397c99d261e22dd64069
shohei-kamimori <shohei_kamimori@dwango.co.jp>
2017-10-10 15:47:42 +0900

    20171010

--- .zsh/enter.zsh
+++ .zsh/enter.zsh
@@ -5,11 +5,14 @@ function do_enter() {
     fi
     echo
     ls -a
+    echo
+    echo
     # ls_abbrev
     if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-        echo
         echo -e "\e[0;33m--- git status ---\e[0m"
         git status -sb
+        echo
+        echo
     fi
     zle reset-prompt
     return 0

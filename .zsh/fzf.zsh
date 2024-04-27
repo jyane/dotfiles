7aebaeec642600df659bf45173547bfb732ba6d2
Shohei Kamimori <jyane.kstm@gmail.com>
2024-04-27 23:34:12 +0900

    20240427

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,44 +1,38 @@
 # Key bindings
 # ------------
-if [[ $- == *i* ]]; then
 
-__fzf_use_tmux__() {
-  [ -n "$TMUX_PANE" ] && [ "${FZF_TMUX:-0}" != 0 ] && [ ${LINES:-40} -gt 15 ]
-}
+export FZF_DEFAULT_OPTS="--height 40%"
 
-__fzfcmd() {
-  __fzf_use_tmux__ &&
-    echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
-}
-
-fzf-cdr-widget () {
-    local dir=( $(cdr -l | awk '{ print $2 }' |
-      FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
-    if [ -n "$dir" ]; then
-      BUFFER="cd ${dir}"
-      zle reset-prompt
-    fi
+# CTRL-T: cdr
+fzf-cdr-widget() {
+  local dir=$(cdr -l | awk '{ print $2 }' | fzf)
+  if [ -n "$dir" ]; then
+    BUFFER="cd ${dir}"
+    zle reset-prompt
+  fi
 }
 zle     -N   fzf-cdr-widget
 bindkey '^T' fzf-cdr-widget
 
-# CTRL-R - Paste the selected command from history into the command line
+# CTRL-R: history search
 fzf-history-widget() {
-  local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases 2> /dev/null
-  selected=( $(fc -rl 1 |
-    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)) )
-  local ret=$?
-  if [ -n "$selected" ]; then
-    num=$selected[1]
-    if [ -n "$num" ]; then
-      zle vi-fetch-history -n $num
-    fi
+  local num=$(fc -rl 1 | fzf --query=${${(qqq)LBUFFER}//\"/} | awk '{ print $1 }')
+  if [ -n "$num" ]; then
+    zle vi-fetch-history -n $num
   fi
   zle reset-prompt
-  return $ret
 }
 zle     -N   fzf-history-widget
 bindkey '^R' fzf-history-widget
 
-fi
+# CTRL-H: find a file
+fzf-find-file() {
+  local res=$(find * | fzf --preview 'head -n 100 {}')
+  if [ -n "$res" ]; then
+    BUFFER="$BUFFER ${res}"
+    zle reset-prompt
+  fi
+}
+zle     -N   fzf-find-file
+bindkey '^H' fzf-find-file

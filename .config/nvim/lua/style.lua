8d791ce67d44da9e606018e4dd801b81cc37ab7f
Shohei Kamimori <skamimori@google.com>
2025-08-26 11:14:27 +0900

    20250826

--- .config/nvim/lua/style.lua
+++ .config/nvim/lua/style.lua
@@ -6,10 +6,8 @@ vim.opt.fillchars = 'eob: ,vert: ,horiz: ,vertleft: ,vertright: ,verthoriz: ,',
 
 require('lualine').setup {
   options = {
-    icons_enabled = true,
     theme = 'catppuccin',
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = '', right = ''},
+    section_separators = { left = '', right = '' },
   },
 }
 vim.api.nvim_set_hl(0, 'WinSeparator', { ctermbg = 235 })
--- .zshrc
+++ .zshrc
@@ -1,6 +1,4 @@
 export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export PAGER=less
 export TERM=xterm-256color
